8c55f04331caee0cf83e2d9787fbb90d38edda43
Neoon <neo@tiefkuehler-madness.me>
2022-06-14 23:31:32 +0200

    update

---
+++ consumerKey.py
@@ -0,0 +1,25 @@
+import json, ovh
+
+with open('config.json') as f:
+    config = json.load(f)
+
+# create a client using configuration
+client = ovh.Client(
+    endpoint=config['endpoint'],
+    application_key=config['application_key'],
+    application_secret=config['application_secret']
+)
+
+# Request RO, /me API access
+ck = client.new_consumer_key_request()
+ck.add_rules(ovh.API_READ_ONLY, "/me")
+
+# Request token
+validation = ck.request()
+
+print("Please visit %s to authenticate" % validation['validationUrl'])
+input("and press Enter to continue...")
+
+# Print nice welcome message
+print("Welcome", client.get('/me')['firstname'])
+print("Btw, your 'consumerKey' is '%s'" % validation['consumerKey'])
