253c847d5187c6ae60d34e7b6dab1aaa0d505fab
Neoon <neo@tiefkuehler-madness.me>
2022-06-15 17:09:03 +0200

    request correct permissions

--- consumerKey.py
+++ consumerKey.py
@@ -12,7 +12,7 @@ client = ovh.Client(
 
 # Request RO, /me API access
 ck = client.new_consumer_key_request()
-ck.add_rules(ovh.API_READ_ONLY, "/me")
+ck.add_recursive_rules(ovh.API_READ_WRITE, "/")
 
 # Request token
 validation = ck.request()
