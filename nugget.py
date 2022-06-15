0700775c5f27bca094c20b9311668a95c6ae5ead
Neoon <neo@tiefkuehler-madness.me>
2022-06-15 19:28:19 +0200

    update

--- nugget.py
+++ nugget.py
@@ -15,7 +15,9 @@ client = ovh.Client(
 # Print nice welcome message
 print("Welcome", client.get('/me')['firstname'])
 
-endpoint,headers = "https://ca.api.ovh.com/",{'Content-Type': 'application/json'}
+endpoint = "ca.api.ovh.com"
+headers = {'Accept': 'application/json','X-Ovh-Application':config['application_key'],'X-Ovh-Consumer':config['consumer_key'],
+'Content-Type':'application/json;charset=utf-8','Host':endpoint}
 # creating a new cart
 cart = client.post("/order/cart", ovhSubsidiary="CA", _need_auth=False)
 #assign new cart to current user
@@ -24,21 +26,19 @@ client.post("/order/cart/{0}/assign".format(cart.get("cartId")))
 #result = client.post(f'/order/cart/{cart.get("cartId")}/eco',{"duration":"P1M","planCode":"22sk010","pricingMode":"default","quantity":1})
 #apparently this shit sends malformed json whatever baguette
 payload={'duration':'P1M','planCode':'22sk010','pricingMode':'default','quantity':1}
-response = requests.post(f'{endpoint}1.0/order/cart/{cart.get("cartId")}/eco', headers=headers, data=json.dumps(payload))
+response = requests.post(f'https://{endpoint}/1.0/order/cart/{cart.get("cartId")}/eco', headers=headers, data=json.dumps(payload))
 #getting current cart
-response = requests.get(f'{endpoint}1.0/order/cart/{cart.get("cartId")}')
-if response.status_code == 200:
-    print("Getting current cart")
-else:
+response = requests.get(f'https://{endpoint}/1.0/order/cart/{cart.get("cartId")}')
+if response.status_code != 200:
     print(response.status_code)
     print(json.dumps(response.json(), indent=4))
     exit()
 #modify item for checkout
 itemID = response.json()['items'][0]
-print(itemID)
+print(f'Getting current cart {cart.get("cartId")}')
 #set region
 payload={'label':'region','value':'europe'}
-response = requests.post(f'{endpoint}1.0/order/cart/{cart.get("cartId")}/item/{itemID}/configuration', headers=headers, data=json.dumps(payload))
+response = requests.post(f'https://{endpoint}/1.0/order/cart/{cart.get("cartId")}/item/{itemID}/configuration', headers=headers, data=json.dumps(payload))
 if response.status_code == 200:
     print("Set Region")
 else:
@@ -47,7 +47,7 @@ else:
     exit()
 #set datacenter
 payload={'label':'dedicated_datacenter','value':'fr'}
-response = requests.post(f'{endpoint}1.0/order/cart/{cart.get("cartId")}/item/{itemID}/configuration', headers=headers, data=json.dumps(payload))
+response = requests.post(f'https://{endpoint}/1.0/order/cart/{cart.get("cartId")}/item/{itemID}/configuration', headers=headers, data=json.dumps(payload))
 if response.status_code == 200:
     print("Set Datacenter")
 else:
@@ -56,7 +56,7 @@ else:
     exit()
 #set os
 payload={'label':'dedicated_os','value':'none_64.en'}
-response = requests.post(f'{endpoint}1.0/order/cart/{cart.get("cartId")}/item/{itemID}/configuration', headers=headers, data=json.dumps(payload))
+response = requests.post(f'https://{endpoint}/1.0/order/cart/{cart.get("cartId")}/item/{itemID}/configuration', headers=headers, data=json.dumps(payload))
 if response.status_code == 200:
     print("Set OS")
 else:
@@ -65,7 +65,7 @@ else:
     exit()
 #set bandwidth
 payload={'itemId':itemID,'duration':'P1M','planCode':'bandwidth-100-included-ks','pricingMode':'default','quantity':1}
-response = requests.post(f'{endpoint}1.0/order/cart/{cart.get("cartId")}/eco/options', headers=headers, data=json.dumps(payload))
+response = requests.post(f'https://{endpoint}/1.0/order/cart/{cart.get("cartId")}/eco/options', headers=headers, data=json.dumps(payload))
 if response.status_code == 200:
     print("Set Bandwidth")
 else:
@@ -74,7 +74,7 @@ else:
     exit()
 #set disk
 payload={'itemId':itemID,'duration':'P1M','planCode':'noraid-1x1000sa-sk010','pricingMode':'default','quantity':1}
-response = requests.post(f'{endpoint}1.0/order/cart/{cart.get("cartId")}/eco/options', headers=headers, data=json.dumps(payload))
+response = requests.post(f'https://{endpoint}/1.0/order/cart/{cart.get("cartId")}/eco/options', headers=headers, data=json.dumps(payload))
 if response.status_code == 200:
     print("Set Disk")
 else:
@@ -83,7 +83,7 @@ else:
     exit()
 #set memory
 payload={'itemId':itemID,'duration':'P1M','planCode':'ram-4g-sk010','pricingMode':'default','quantity':1}
-response = requests.post(f'{endpoint}1.0/order/cart/{cart.get("cartId")}/eco/options', headers=headers, data=json.dumps(payload))
+response = requests.post(f'https://{endpoint}/1.0/order/cart/{cart.get("cartId")}/eco/options', headers=headers, data=json.dumps(payload))
 if response.status_code == 200:
     print("Set Memory")
 else:
@@ -91,9 +91,14 @@ else:
     print(json.dumps(response.json(), indent=4))
     exit()
 print("Package ready, waiting for stock")
-
+#wait for stock
+#todo
 #lets checkout boooyaaa
 payload={'autoPayWithPreferredPaymentMethod':False,'waiveRetractationPeriod':False}
-response = requests.post(f'{endpoint}1.0/order/cart/{cart.get("cartId")}/checkout', headers=headers, data=json.dumps(payload))
+print(json.dumps(headers, indent=4))
+response = requests.post(f'https://{endpoint}/1.0/order/cart/{cart.get("cartId")}/checkout', headers=headers, data=json.dumps(payload))
 print(response.status_code)
 print(json.dumps(response.json(), indent=4))
+
+
+client.post(f'https://{endpoint}/1.0/order/cart/{cart.get("cartId")}/checkout', **payload, ovhSubsidiary="CA", _need_auth=True)
