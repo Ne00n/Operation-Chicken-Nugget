39bd618655ef7ab1b3ff28ad0d71032783faf9a5
Neoon <neo@tiefkuehler-madness.me>
2025-06-25 21:10:10 +0200

    also remove that check

--- nugget.py
+++ nugget.py
@@ -162,7 +162,7 @@ while True:
         print(f'Run {check+1} {now.strftime("%H:%M:%S")}')
         #wait for stock
         try:
-            response = requests.get(f'{selectedEndpoint["availability"]}?excludeDatacenters=false&planCode={planConfig["planCode"]}&server={planConfig["planCode"]}')
+            response = requests.get(f'{selectedEndpoint["availability"]}?excludeDatacenters=false&planCode={planConfig["planCode"]}')
         except Exception as e:
             print(f"Failed to fetch stock got error '{e}' retrying...")
             time.sleep(2)
