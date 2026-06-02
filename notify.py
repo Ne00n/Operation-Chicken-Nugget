a6fca51f994cc573fdc64cd363c660faaf50dc0a
Ne00n <neo@tiefkuehler-madness.me>
2026-06-02 13:59:48 +0200

    way better

--- notify.py
+++ notify.py
@@ -17,7 +17,7 @@ if not "endpoint" in config:
 with open(f"{path}/config.json", 'w') as f: json.dump(config, f, indent=4)
 if not config['endpoint'] or not config['gotify']: exit()
 
-selectedEndpoint = config['endpoint']
+selectedEndpoint = endpoints[config['endpoint']]
 
 client = ovh.Client(
     endpoint=selectedEndpoint['endpoint'],
@@ -26,15 +26,32 @@ client = ovh.Client(
     consumer_key=config['consumer_key'],
 )
 
-result = client.get("/dedicated/server")
-for dedi in result:
+dedis = client.get("/dedicated/server")
+for dedi in dedis:
+    print(f"Checking {dedi}")
     if not dedi in history:
+        print(f"New dedi detected: {dedi}")
+        history[dedi] = {"detected":int(time.time()),"notified":0,"errors":{"hardware":0}}
+
+    if not "hardware" in history[dedi] and history[dedi]['errors']['hardware'] <= 2:
+        try:
+            hardware = client.get(f"/dedicated/server/{dedi}/specifications/hardware")
+            history[dedi]['hardware'] = hardware
+        except Exception as e:
+            print(f"Failed to fetch hardware got error {e}")
+            history[dedi]['errors']['hardware'] += 1
+            continue
+    elif history[dedi]['errors']['hardware'] > 2:
+        history[dedi]['hardware'] = "Unable to fetch hardware."
+    
+    if not history[dedi]['notified']:
         for run in range(3):
             try:
-                requests.post(config['gotify'], json={"message": dedi,"priority": 10,"title": "New baguette detected"})
+                requests.post(config['gotify'], json={"message": json.dumps(history[dedi]['hardware'], indent=4),"priority": 10,"title": f"New baguette detected {dedi}"})
+                history[dedi]['notified'] = 1
                 break
             except Exception as e:
                 print(f"Failed to post {config['gotify']} got error '{e}' retrying...")
                 time.sleep(5)
 
-with open(f"{path}/history.json", 'w') as f: json.dump(result, f, indent=4)
+with open(f"{path}/history.json", 'w') as f: json.dump(history, f, indent=4)
